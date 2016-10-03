bd434ed52c6c8e339a8bf7ab47c84e280799f064
callejac <clint.calleja@tipico.com>
2016-10-03 11:52:28 +0200

    Get live data every 5 seconds

--- src/main/java/com/tipico/livescore/Application.java
+++ src/main/java/com/tipico/livescore/Application.java
@@ -1,9 +1,18 @@
 package com.tipico.livescore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Configuration
+@EnableAsync
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
